d98d655418c7e064650ca6787fbef796d7298935
TangNV <nvtang.khtn@gmail.com>
2015-12-28 16:45:16 +0700

    #4 add field item_number to Dish model, add get_menu_service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228073546) do
+ActiveRecord::Schema.define(version: 20151228094240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151228073546) do
   create_table "dishes", force: :cascade do |t|
     t.string  "name"
     t.decimal "price"
+    t.integer "item_number"
   end
 
   create_table "order_items", force: :cascade do |t|
