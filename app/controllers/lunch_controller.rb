c061b0cd9271e9be477f66a6f297e7f6d9e969f2
TangNV <nvtang.khtn@gmail.com>
2015-12-28 14:36:47 +0700

    #2 refactor function name

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -13,9 +13,4 @@ class LunchController < ApplicationController
     #check full users order to order lunch
     render status: 200, json: "order"
   end
-
-  def notify
-    #call slack api to post success message to lunch channel
-    render status: 200, json: "notify"
-  end
 end
