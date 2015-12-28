0c3da95219e66e257da95836fc764e11a9183d04
Linh Nguyen <linhvn09@gmail.com>
2015-12-28 17:46:24 +0700

    config time zone to set time for scheduler

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module OrderLunch
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.time_zone = 'London'
   end
 end
