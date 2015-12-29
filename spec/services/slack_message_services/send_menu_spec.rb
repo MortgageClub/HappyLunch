7681b5e395872579e58e7525f0d8b2c95a99a4d4
Cuong Vu <cuongvu0103@gmail.com>
2015-12-29 15:09:53 +0700

    Fix rubocop

--- spec/services/slack_message_services/send_menu_spec.rb
+++ spec/services/slack_message_services/send_menu_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 describe SlackMessageServices::SendMenu do
-  let!(:tofu) {Dish.create(name: "Tofu", price: 12)}
-  let!(:fruit) {Dish.create(name: "Fruit", price: 15)}
+  let!(:tofu) { Dish.create(name: "Tofu", price: 12) }
+  let!(:fruit) { Dish.create(name: "Fruit", price: 15) }
 
   it "Sends all list of dishes to lunch channel" do
     VCR.use_cassette("list_of_dishes") do
