22375912e8f3810135bbe720b44fd2edc973f954
TangNV <nvtang.khtn@gmail.com>
2015-12-29 13:43:09 +0700

    validate dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,8 @@
 class Dish < ActiveRecord::Base
   validates :name, presence: true
-  has_many :order_items
+  validates :price, presence: true
+  validates :item_number, presence: true
 
+  has_many :order_items
   scope :today, -> { where.not(item_number: nil) }
 end
