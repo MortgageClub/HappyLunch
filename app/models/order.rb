1b4a79125f84567ce4e9303de6684ef05049c2b3
Linh Nguyen <linhvn09@gmail.com>
2015-12-29 10:54:09 +0700

    implement the remind method in order class

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,17 @@ class Order < ActiveRecord::Base
   has_many :order_items
 
   scope :today, -> { where("created_at >= ?", Time.zone.now.beginning_of_day) }
+
+  def self.today_order_completed?
+    @today_order = self.today.first
+    GetListMembersService.call.uniq.count == @today_order.order_items.pluck(:username).uniq.count
+  end
+
+  def self.notify_user
+    @today_order = self.today.first
+    unorder_members = GetListMembersService.call.uniq - @today_order.order_items.pluck(:username).uniq
+    unorder_members.each do |member|
+      SendMessageToSlackService.call("#lunch", "Hey @#{member}! You haven't ordered lunch yet. Please order now!", link_names: 1)
+    end
+  end
 end
--- lib/tasks/slack.rake
+++ lib/tasks/slack.rake
@@ -2,7 +2,8 @@ namespace :slack do
   desc "Notify Lunch Menu"
   task :notify_lunch_menu => :environment do
     puts "Notifying lunch menu job is running"
-    SendMessageToSlackService.call("#lunch")
+    GetMenuService.call
+    SendMessageToSlackService.post_lunch_menu_to_slack_lunch("#lunch")
     puts "Notifying lunch is running on background"
   end
 end
