f3f060a19dfc3f19070c01fdd383790990e2cfb5
Linh Nguyen <linhvn09@gmail.com>
2015-12-29 10:58:23 +0700

    implement rake task for notifying users have not ordered lunch

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,11 +8,13 @@ class Order < ActiveRecord::Base
     GetListMembersService.call.uniq.count == @today_order.order_items.pluck(:username).uniq.count
   end
 
-  def self.notify_user
+  def self.notify_users
     @today_order = self.today.first
-    unorder_members = GetListMembersService.call.uniq - @today_order.order_items.pluck(:username).uniq
-    unorder_members.each do |member|
-      SendMessageToSlackService.call("#lunch", "Hey @#{member}! You haven't ordered lunch yet. Please order now!", link_names: 1)
+    if !today_order_completed?
+      unorder_members = GetListMembersService.call.uniq - @today_order.order_items.pluck(:username).uniq
+      unorder_members.each do |member|
+        SendMessageToSlackService.call("#lunch", "Hey @#{member}! You haven't ordered lunch yet. Please order now!", link_names: 1)
+      end
     end
   end
 end
--- lib/tasks/slack.rake
+++ lib/tasks/slack.rake
@@ -6,4 +6,9 @@ namespace :slack do
     SendMessageToSlackService.post_lunch_menu_to_slack_lunch("#lunch")
     puts "Notifying lunch is running on background"
   end
+  task :notify_user_order => :environment do
+    puts "Notifying user has not ordered lunch job is running"
+    Order.notify_users
+    puts "Notifying user has not ordered lunch job is finish"
+  end
 end
