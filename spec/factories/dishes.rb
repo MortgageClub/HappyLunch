ea134e95c7a5447e0366261f77e09261f62a50b3
Cuong Vu <cuongvu0103@gmail.com>
2015-12-29 10:19:33 +0700

    Add tests for SendSuccessMessage service

--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :dish do |f|
     f.name { FFaker::Food.ingredient }
-    f.price { rand(100000) }
+    f.price { rand(100_000) }
     f.item_number { rand(100) }
   end
 end
