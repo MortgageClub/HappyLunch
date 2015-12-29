25098d07ca26b5adaaf2a1e953f510bfaadcdfb1
Linh Nguyen <linhvn09@gmail.com>
2015-12-29 12:04:43 +0700

    refactor code

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,19 +4,17 @@ class Order < ActiveRecord::Base
   scope :today, -> { where("created_at >= ?", Time.zone.now.beginning_of_day) }
 
   def self.today_order_completed?
-    @today_order = self.today.first
-    return false unless @today_order
-    GetListMembersService.call.uniq.count == @today_order.order_items.pluck(:username).uniq.count
+    return false unless self.today.first
+    GetListMembersService.call.uniq.count == self.today.first.order_items.pluck(:username).uniq.count
   end
 
   def self.notify_users
-    if !today_order_completed?
-      @today_order = self.today.first
-      if @today_order
-        unorder_members = GetListMembersService.call.uniq - @today_order.order_items.pluck(:username).uniq
-        unorder_members.each do |member|
-          SendMessageToSlackService.call("#lunch", "Hey @#{member}! You haven't ordered lunch yet. Please order now!", link_names: 1)
-        end
+    return if today_order_completed?
+    @today_order = self.today.first
+    if @today_order
+      unorder_members = GetListMembersService.call.uniq - @today_order.order_items.pluck(:username).uniq
+      unorder_members.each do |member|
+        SendMessageToSlackService.call("#lunch", "Hey @#{member}! You haven't ordered lunch yet. Please order now!", link_names: 1)
       end
     end
   end
