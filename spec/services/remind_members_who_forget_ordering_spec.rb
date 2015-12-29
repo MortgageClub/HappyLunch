880298257f74d3a6536e1b1eff8777fa8dbe314c
Linh Nguyen <linhvn09@gmail.com>
2015-12-29 17:55:53 +0700

    modify describe message

--- spec/services/remind_members_who_forget_ordering_spec.rb
+++ spec/services/remind_members_who_forget_ordering_spec.rb
@@ -8,7 +8,7 @@ describe SlackMessageServices::RemindMembersWhoForgetOrdering do
     let!(:fruit) { FactoryGirl.create(:dish, name: "Fruit", price: 15, item_number: 2) }
     let!(:order_item1) { FactoryGirl.create(:order_item, order: today_order, username: "tangnv", dish: tofu)}
 
-    it "Sends all list of dishes to lunch channel" do
+    it "Sends notice message to users have not ordered" do
       VCR.use_cassette("users_have_not_order") do
         allow(GetListMembersService).to receive(:call).and_return(%w(billy cuongvu linhvn09 tangnv))
         expect(SlackMessageServices::Base).to receive(:send_message)
