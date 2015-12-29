da80436433bafb6944b4c5034bb31d2c60eb0c22
Linh Nguyen <linhvn09@gmail.com>
2015-12-29 11:11:06 +0700

    catch error if order for today not exist

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,16 +4,19 @@ class Order < ActiveRecord::Base
   scope :today, -> { where("created_at >= ?", Time.zone.now.beginning_of_day) }
 
   def self.today_order_completed?
+    return false unless @today_order
     @today_order = self.today.first
     GetListMembersService.call.uniq.count == @today_order.order_items.pluck(:username).uniq.count
   end
 
   def self.notify_users
-    @today_order = self.today.first
     if !today_order_completed?
-      unorder_members = GetListMembersService.call.uniq - @today_order.order_items.pluck(:username).uniq
-      unorder_members.each do |member|
-        SendMessageToSlackService.call("#lunch", "Hey @#{member}! You haven't ordered lunch yet. Please order now!", link_names: 1)
+      @today_order = self.today.first
+      if @today_order
+        unorder_members = GetListMembersService.call.uniq - @today_order.order_items.pluck(:username).uniq
+        unorder_members.each do |member|
+          SendMessageToSlackService.call("#lunch", "Hey @#{member}! You haven't ordered lunch yet. Please order now!", link_names: 1)
+        end
       end
     end
   end
