8eb7d5583df1372392014573c52945bf4fc907b9
Linh Nguyen <linhvn09@gmail.com>
2015-12-29 17:01:52 +0700

    modify test case to fix the value of dish

--- spec/services/slack_message_services/send_menu_spec.rb
+++ spec/services/slack_message_services/send_menu_spec.rb
@@ -1,12 +1,20 @@
 require "rails_helper"
 
 describe SlackMessageServices::SendMenu do
-  let!(:tofu) { Dish.create(name: "Tofu", price: 12) }
-  let!(:fruit) { Dish.create(name: "Fruit", price: 15) }
+  let!(:tofu) { FactoryGirl.create(:dish, name: "Tofu", price: 12, item_number: 1) }
+  let!(:fruit) { FactoryGirl.create(:dish, name: "Fruit", price: 15, item_number: 2) }
 
-  it "Sends all list of dishes to lunch channel" do
-    VCR.use_cassette("list_of_dishes") do
-      expect(described_class.call).to be_truthy
+  describe ".call" do
+    it "Sends all list of dishes to lunch channel" do
+      VCR.use_cassette("list_of_dishes") do
+        expect(described_class.call).to be_truthy
+      end
+    end
+  end
+
+  describe ".format_content" do
+    it "contains the dish available today" do
+      expect(described_class.format_content).to include "Tofu"
     end
   end
 end
