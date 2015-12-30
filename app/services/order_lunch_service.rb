ebb512b53d95e95db885ece604da9caa69ecccf1
Cuong Vu <cuongvu0103@gmail.com>
2015-12-30 09:53:50 +0700

    Update right success message

--- app/services/order_lunch_service.rb
+++ app/services/order_lunch_service.rb
@@ -79,8 +79,7 @@ class OrderLunchService
   end
 
   def check_status
-    success_message = crawler.all(".report", text: "Cám ơn quý khách đã quan tâm đến dịch vụ của chúng tôi!
-      Yêu cầu của quý khách sẽ được phản hồi trong thời gian sớm nhất.")[0]
+    success_message = crawler.all(".report", text: "Cám ơn quý khách đã quan tâm đến dịch vụ của chúng tôi! Yêu cầu của quý khách sẽ được phản hồi trong thời gian sớm nhất.")[0]
     success_message ? @result = true : @result = false
   end
 end
