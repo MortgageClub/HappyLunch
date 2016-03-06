e6a2f017661cedc14dda918ea02a4d7df669c487
Cuong Vu <cuongvu0103@gmail.com>
2016-03-06 21:41:36 +0700

    Refactor GetMenuSerivce
    Why:
    * GetMenuService should do only 1 job: get menu.
    * It doesn't need to create menu
    
    This change addresses the need by:
    * GetMenuService

--- app/services/get_menu_service.rb
+++ app/services/get_menu_service.rb
@@ -4,30 +4,18 @@ require 'open-uri'
 class GetMenuService
   def self.call
     item_number = 0
-    init_order
+    dishes = []
     doc = get_html('http://giachanhcamtuyet.com.vn/index.php?m=dat-tiec&id=12&t=orders&s=1')
 
-    get_elements(doc, "div.list_item").each do |item|
+    doc.css("div.list_item").each do |item|
       name_element = get_element(item, "div.name")
       price_element = get_element(item, "div.price input")
 
       next unless valid_dish?(price_element["value"].to_f)
 
-      item_number += 1
-      insert_or_update_dish(name_element.text, price_element["value"].to_f, item_number)
+      dishes << {name: name_element.text, price: price_element["value"].to_f, number: item_number += 1}
     end
-  end
-
-  def self.insert_or_update_dish(name, price, item_number)
-    dish = Dish.find_or_initialize_by(name: name)
-    dish.price = price
-    dish.item_number = item_number
-    dish.save
-  end
-
-  def self.init_order
-    Order.create(order_date: Time.zone.now) unless Order.today.first
-    Dish.update_all(item_number: nil)
+    dishes
   end
 
   def self.get_html(url)
@@ -36,10 +24,6 @@ class GetMenuService
 
   def self.get_element(html, selector)
     html.at_css(selector)
-  end
-
-  def self.get_elements(html, selector)
-    html.css(selector)
   end
 
   def self.valid_dish?(price)
